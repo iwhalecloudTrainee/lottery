877614667635bf32b18f72e8351faa9a59523687
W4i <951308393@qq.com>
2021-01-17 23:40:50 +0800

    一些奇奇怪怪的路径、名字修改

--- src/main/java/com/iwhalecloud/lottery/routers/Router.java
+++ src/main/java/com/iwhalecloud/lottery/routers/Router.java
@@ -1,4 +1,4 @@
-package com.iwhalecloud.lottery.router;
+package com.iwhalecloud.lottery.routers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/iwhalecloud/lottery/utils/ExcelData.java
+++ src/main/java/com/iwhalecloud/lottery/utils/ExcelData.java
@@ -1,4 +1,4 @@
-package com.iwhalecloud.lottery.util;
+package com.iwhalecloud.lottery.utils;
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
