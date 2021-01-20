6c44f6a186ee93b28b7fb582ae8430da26bd4209
PeiHongjun <863741266@qq.com>
2021-01-20 15:22:53 +0800

    music bug

--- src/main/resources/static/lottery/lottery.js
+++ src/main/resources/static/lottery/lottery.js
@@ -17,7 +17,8 @@ new Vue({
             lotteryId: 0,
             prizeId: 0,
             staffName: '',
-        }
+        },
+        audio:new Audio("resources/bgm.mp3"),//这里的路径写上mp3文件在项目中的绝对路径
     },
     mounted() {
         this.getLotteryId();
@@ -35,6 +36,7 @@ new Vue({
                 if (res.data.success) {
                     this.lotteryName = res.data.data.lotteryName;
                     this.prizeList = res.data.data.prizeList;
+                    this.prizeId=''
                     console.log(this.prizeList)
                 }
             })
@@ -45,25 +47,55 @@ new Vue({
         lottery: function () {
             var that = this;
             if (!this.prizeId) {
-                alert("请选择抽奖项目")
+                alert("请选择抽奖项目");
                 return;
             } else {
                 if (this.isLottery == "开始抽奖") {
+                    that.audio.play();
+                    console.log(that.audio);
+                    console.log("抽奖开始时间",new Date());
                     that.timeOutNub = setTimeout(function stopLottery() {
                         that.autoplay = false;
                         that.isLottery = "开始抽奖";
-                        that.awardData.staffName = document.getElementsByClassName('is-active')[0].outerText;
-                        that.setLottery();
-                    }, 15000);
+                        that.audio.pause();
+                        console.log("定时结束",new Date());
+                    }, 3000);
                     this.isLottery = "停止抽奖";
                     that.autoplay = true;
-                } else {
+                }else {
+                    that.audio.pause();
+                    console.log(that.timeOutNub);
                     clearTimeout(that.timeOutNub);
+                    console.log("抽奖结束时间",new Date());
                     this.isLottery = "开始抽奖";
                     that.autoplay = false;
+                    console.log(this.staffList);
                     this.awardData.staffName = document.getElementsByClassName('is-active')[0].outerText;
                     this.setLottery();
                 }
+                //判断是否正在滚动
+
+                // if (that.autoplay) {
+                //     that.autoplay = false;
+                //     that.audio.pause();//暂停播放
+                //     console.log(this.staffList);
+                //     this.awardData.staffName = document.getElementsByClassName('is-active')[0].outerText;
+                //     this.setLottery();
+                // } else {
+                //     that.autoplay = true;
+                //     that.audio.play();//播放
+                //     if (that.autoplay) {
+                //         that.autoplay = false;
+                //         that.audio.pause();//暂停播放
+                //     }
+                // }
+                //
+                // setTimeout(function () {
+                //     that.audio.pause();
+                // }, 15000);
+                //
+                // //抽奖结束后，给this.awardData赋值,然后调用
+                // this.setLottery();
             }
         },
 
