04083611409ada53f5351854559aa1da683a2226
macw <ma.chiwei@iwhalecloud.com>
2021-03-16 16:21:46 +0800

    转cdn，打war包

--- src/main/java/com/iwhalecloud/lottery/LotteryApplication.java
+++ src/main/java/com/iwhalecloud/lottery/LotteryApplication.java
@@ -2,14 +2,20 @@ package com.iwhalecloud.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.iwhalecloud.lottery.mapper")
-public class LotteryApplication {
+public class LotteryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotteryApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder.sources(LotteryApplication.class);
 	}
 
 }
--- src/main/resources/static/vue/filter/vue-filter.js
+++
@@ -1,9 +0,0 @@
-// 百分比过滤
-Vue.filter('leaderRateFilter', function (val) {
-    if (val == undefined || val == '') {
-        return '--';
-    }
-    // 占比保留一位小数
-    var numValue = Number(val).toFixed(2);
-    return numValue + '%';
-})
